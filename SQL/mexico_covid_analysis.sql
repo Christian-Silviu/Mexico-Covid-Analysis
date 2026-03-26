e50e1f9d052957d46d9fa54345f1612624cc34da
Christian <116528383+Christian-Silviu@users.noreply.github.com>
2026-03-26 15:26:11 -0400

    Update mexico_covid_analysis.sql
    Complete SQL Analysis Queries

--- SQL/mexico_covid_analysis.sql
+++ SQL/mexico_covid_analysis.sql
@@ -139,7 +139,7 @@
 -- 	ELSE TO_DATE(date_died, 'DD/MM/YYYY')
 -- END;
 
--- -- Fix column name typos
+-- Fix column name typos
 -- ALTER TABLE covid_patients
 -- RENAME COLUMN hipertension TO hypertension;
 
@@ -166,65 +166,94 @@
 -- ==============
 
 -- High Level Overview
--- SELECT COUNT(*) AS total_patients,
--- 	COUNT(CASE WHEN date_died IS NOT NULL THEN 1 END) AS total_dead,
--- 	COUNT(CASE WHEN patient_type = 'Hospitalized' THEN 1 END) AS total_hospitalized,
--- 	COUNT(CASE WHEN classification_final <= 3 THEN 1 END) AS confirmed_cases,
--- 	ROUND(COUNT(CASE WHEN date_died IS NOT NULL THEN 1 END)::NUMERIC / COUNT(*) * 100, 2) AS mortality_rate
--- FROM covid_patients;
+SELECT COUNT(*) AS total_patients,
+	COUNT(CASE WHEN date_died IS NOT NULL THEN 1 END) AS total_dead,
+	COUNT(CASE WHEN patient_type = 'Hospitalized' THEN 1 END) AS total_hospitalized,
+	COUNT(CASE WHEN classification_final <= 3 THEN 1 END) AS confirmed_cases,
+	ROUND(COUNT(CASE WHEN date_died IS NOT NULL THEN 1 END)::NUMERIC / COUNT(*) * 100, 2) AS mortality_rate
+FROM covid_patients;
 
-
--- Age Group Distribution
--- SELECT
--- 	CASE WHEN age <= 17 THEN 'Minors (0-17)'
--- 		WHEN age BETWEEN 18 AND 34 THEN 'Young Adults (18-34)'
--- 		WHEN age BETWEEN 35 and 49 THEN 'Middle Aged (35-49)'
--- 		WHEN AGE BETWEEN 50 AND 64 THEN 'Older Adults (50-64)'
--- 		WHEN AGE BETWEEN 65 AND 79 THEN 'Seniors (65-79)'
--- 		WHEN AGE >= 80 THEN 'Elderly (80+)'
--- 	END AS age_group,
--- 	COUNT(*) AS patient_count
--- FROM covid_patients
--- GROUP BY age_group
--- ORDER BY MIN(age);
-
--- Expanded Group Distribution Analysis
--- SELECT
--- 	CASE WHEN age <= 17 THEN 'Minors (0-17)'
--- 		WHEN age BETWEEN 18 AND 34 THEN 'Young Adults (18-34)'
--- 		WHEN age BETWEEN 35 and 49 THEN 'Middle Aged (35-49)'
--- 		WHEN AGE BETWEEN 50 AND 64 THEN 'Older Adults (50-64)'
--- 		WHEN AGE BETWEEN 65 AND 79 THEN 'Seniors (65-79)'
--- 		WHEN AGE >= 80 THEN 'Elderly (80+)'
--- 	END AS age_group,
--- 	COUNT(*) AS patient_count,
--- 	COUNT(CASE WHEN date_died IS NOT NULL THEN 1 END) as total_deaths,
--- 	ROUND(COUNT(CASE WHEN date_died IS NOT NULL THEN 1 END)::NUMERIC / COUNT(*) * 100, 2) AS mortality_rate
--- FROM covid_patients
--- GROUP BY age_group
--- ORDER BY MIN(age);
+-- Age Group Distribution Analysis
+SELECT
+	CASE WHEN age <= 17 THEN 'Minors (0-17)'
+		WHEN age BETWEEN 18 AND 34 THEN 'Young Adults (18-34)'
+		WHEN age BETWEEN 35 and 49 THEN 'Middle Aged (35-49)'
+		WHEN AGE BETWEEN 50 AND 64 THEN 'Older Adults (50-64)'
+		WHEN AGE BETWEEN 65 AND 79 THEN 'Seniors (65-79)'
+		WHEN AGE >= 80 THEN 'Elderly (80+)'
+	END AS age_group,
+	COUNT(*) AS patient_count,
+	COUNT(CASE WHEN date_died IS NOT NULL THEN 1 END) as total_deaths,
+	ROUND(COUNT(CASE WHEN date_died IS NOT NULL THEN 1 END)::NUMERIC / COUNT(*) * 100, 2) AS mortality_rate
+FROM covid_patients
+GROUP BY age_group
+ORDER BY MIN(age);
 
 -- Gender Based Mortality Rate Analysis
--- SELECT
--- 	sex AS Gender,
--- 	COUNT(*) AS patient_count,
--- 	COUNT(CASE WHEN date_died IS NOT NULL THEN 1 END) AS total_deaths,
--- 	ROUND(COUNT(CASE WHEN date_died IS NOT NULL THEN 1 END)::NUMERIC / COUNT(*) * 100 , 2) AS Mortality_Rate
--- 	FROM covid_patients
--- 	GROUP BY sex;
+SELECT
+	sex AS Gender,
+	COUNT(*) AS patient_count,
+	COUNT(CASE WHEN date_died IS NOT NULL THEN 1 END) AS total_deaths,
+	ROUND(COUNT(CASE WHEN date_died IS NOT NULL THEN 1 END)::NUMERIC / COUNT(*) * 100 , 2) AS Mortality_Rate
+	FROM covid_patients
+	GROUP BY sex;
 
+-- Mortality Rate by Comorbidity
+SELECT
+	comorbidity,
+	COUNT(*) as patient_count,
+	COUNT(CASE WHEN date_died IS NOT NULL THEN 1 END) as total_deaths,
+	ROUND(COUNT(CASE WHEN date_died IS NOT NULL THEN 1 END)::NUMERIC / COUNT(*) * 100, 2) as mortality_rate
+FROM covid_patients
+CROSS JOIN UNNEST(ARRAY['diabetes','obesity','hypertension','cardiovascular', 'pneumonia','copd','asthma','tobacco','renal_chronic','inmsupr']) AS comorbidity
+WHERE CASE comorbidity
+	WHEN 'diabetes' THEN diabetes
+	WHEN 'obesity' THEN obesity
+	WHEN 'hypertension' THEN hypertension
+	WHEN 'cardiovascular' THEN cardiovascular
+	WHEN 'pneumonia' THEN pneumonia
+	WHEN 'copd' THEN copd
+	WHEN 'asthma' THEN asthma
+	WHEN 'tobacco' THEN tobacco
+	WHEN 'renal_chronic' THEN renal_chronic
+	WHEN 'inmsupr' THEN inmsupr
+END = TRUE
+GROUP BY comorbidity
+ORDER BY mortality_rate DESC;
 
+-- Intubation and ICU rate (Confirmed Cases)
+SELECT
+	COUNT(*) as patient_count,
+	COUNT(CASE WHEN icu IS true THEN 1 END) as total_icu,
+	ROUND(COUNT(CASE WHEN icu IS true THEN 1 END)::NUMERIC / COUNT(*) * 100, 2) as icu_rate,
+	COUNT(CASE WHEN intubed IS true THEN 1 END) as total_intubed,
+	ROUND(COUNT(CASE WHEN intubed IS true THEN 1 END)::NUMERIC / COUNT(*) * 100, 2) as intubed_rate
+FROM covid_patients
+WHERE classification_final <= 3;
 
+-- Hospitalization Rate by Age Analysis
+SELECT
+	CASE WHEN age <= 17 THEN 'Minors (0-17)'
+		WHEN age BETWEEN 18 AND 34 THEN 'Young Adults (18-34)'
+		WHEN age BETWEEN 35 and 49 THEN 'Middle Aged (35-49)'
+		WHEN age BETWEEN 50 AND 64 THEN 'Older Adults (50-64)'
+		WHEN age BETWEEN 65 AND 79 THEN 'Seniors (65-79)'
+		WHEN age >= 80 THEN 'Elderly (80+)'
+	END AS age_group,
+	COUNT(*) as patient_count,
+	COUNT(CASE WHEN patient_type = 'Hospitalized' THEN 1 END) as total_hospitalized,
+	ROUND(COUNT(CASE WHEN patient_type = 'Hospitalized' THEN 1 END)::NUMERIC / COUNT(*) * 100, 2) as hospitalization_rate
+FROM covid_patients
+GROUP BY age_group
+ORDER BY MIN(age);
 
-
-
-
-
-
-
-
-
-
-
-
-
+-- Mortality Rate per Total Number of Comorbidities
+SELECT 
+	(diabetes::INT + obesity::INT + hypertension::INT + cardiovascular::INT + pneumonia::INT + copd::INT + 
+	asthma::INT + tobacco::INT + renal_chronic::INT + inmsupr::INT) AS num_comorbidities,
+	COUNT(*) as patient_count,
+	COUNT(CASE WHEN date_died IS NOT NULL THEN 1 END) as total_deaths,
+	ROUND(COUNT(CASE WHEN date_died IS NOT NULL THEN 1 END)::NUMERIC / COUNT(*) * 100, 2) as mortality_rate
+FROM covid_patients
+GROUP BY num_comorbidities
+ORDER BY num_comorbidities ASC;
